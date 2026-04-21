7954c33810a79ae28024da78728de095ba0d1fea
Bane Sullivan <22067021+banesullivan@users.noreply.github.com>
2026-04-20 19:32:25 -0700

    whoops, vibed too hard

--- tests/test_set_cpos.py
+++ tests/test_set_cpos.py
@@ -47,36 +47,6 @@ def test_set_cpos_yx_places_camera_on_negative_z(renderer):
     assert cam.position[2] < cam.focal_point[2]
 
 
-def test_set_cpos_plus_x_places_camera_on_positive_x(renderer):
-    renderer.set_cpos('+x')
-    cam = renderer._plotter.camera
-    assert cam.position[0] > cam.focal_point[0]
-    assert abs(cam.position[1] - cam.focal_point[1]) < 1e-6
-    assert abs(cam.position[2] - cam.focal_point[2]) < 1e-6
-    assert abs(cam.up[2] - 1.0) < 1e-6
-
-
-def test_set_cpos_bare_x_aliases_plus_x(renderer):
-    renderer.set_cpos('x')
-    cam = renderer._plotter.camera
-    assert cam.position[0] > cam.focal_point[0]
-    assert abs(cam.up[2] - 1.0) < 1e-6
-
-
-def test_set_cpos_minus_x_places_camera_on_negative_x(renderer):
-    renderer.set_cpos('-x')
-    cam = renderer._plotter.camera
-    assert cam.position[0] < cam.focal_point[0]
-    assert abs(cam.up[2] - 1.0) < 1e-6
-
-
-def test_set_cpos_plus_z_places_camera_on_positive_z(renderer):
-    renderer.set_cpos('+z')
-    cam = renderer._plotter.camera
-    assert cam.position[2] > cam.focal_point[2]
-    assert abs(cam.up[1] - 1.0) < 1e-6
-
-
 def test_set_cpos_marks_dirty(renderer):
     renderer.render_frame()
     assert not renderer.is_dirty
